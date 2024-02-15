f752e78f87914692ecab8a5441cd33b562aac738
evoevoevo <evrencan.9@outlook.com>
2024-02-15 19:36:33 +0300

    updated with .env

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'your_mysql_user',
-  password: 'your_mysql_password',
-  database: 'your_database_name',
+  host:  process.env.DB_HOST,
+  user:  process.env.DB_USER,
+  password:  process.env.DB_PASSWORD,
+  database:  process.env.DB_DATABASE,
 });
 
 db.connect((err) => {
